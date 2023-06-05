b599be20e2e80b2d4f84bfe31aba7c804ad5d4ca
SSSanga <ksa901125@gmail.com>
2023-06-05 17:51:08 +0900

    MEMBERS_DML를 위해 각 테이블의 고유 프라이머키만 먼저 만들기.

--- MEMBERS/MEMBERS_DML.sql
+++ MEMBERS/MEMBERS_DML.sql
@@ -1,15 +1,15 @@
--- GENDER_CODE
+-- GENDER CODE
 INSERT INTO gender
-(GENDER, GENDER_ID)
+(GENDER,GENDER_ID)
 VALUES
 ('남자','G-03');
 
 INSERT INTO gender
-(GENDER, GENDER_ID)
+(GENDER,GENDER_ID)
 VALUES
 ('여자','G-07');
 
--- ADDRESS
+-- ADDRESS CODE
 INSERT INTO address
 (ADDRESS,ADDRESS_ID)
 VALUES
@@ -22,33 +22,7 @@ VALUES
 ('대전시','A-07'),
 ('울산시','A-08');
 
--- MEM_INFORM
-INSERT INTO mem_inform
-(NAME,AGE,PHONE,UNIQUE_ID,GENDER_ID,ADDRESS_ID)
-VALUES
-('홍길동',25,'010-1234-5678','M-01','G-03','A-01'),
-('김영희',30,'010-9876-5432','M-02','G-07','A-02'),
-('이철수',28,'010-5555-7777','M-03','G-03','A-03'),
-('박지원',23,'010-2222-3333','M-04','G-07','A-04'),
-('최성민',26,'010-4444-6666','M-05','G-03','A-05'),
-('김철호',32,'010-7777-8888','M-06','G-03','A-06'),
-('이지은',29,'010-9999-1111','M-07','G-07','A-07'),
-('정민준',27,'010-2222-3333','M-08','G-03','A-08');
-
--- LOGIN
-INSERT INTO login
-(EMAIL,UNIQUE_ID,LOGIN_ID)
-VALUES
-('honggildong@gmail.com','M-01','L-01'),
-('kimyounghee@gmail.com','M-02','L-02'),
-('leechulsu@gmail.com','M-03','L-03'),
-('parkjiwon@gmail.com','M-04','L-04'),
-('choiseongmin@gmail.com','M-05','L-05'),
-('kimcheolho@gmail.com','M-06','L-06'),
-('leejiun@gmail.com','M-07','L-07'),
-('jeongminjun@gmail.com','M-08' ,'L-08');
-
--- HOBBY_TYPE
+-- HOBBY CODE
 INSERT INTO hobby_type
 (HOBBY,HOBBY_TYPE_ID)
 VALUES
@@ -65,7 +39,33 @@ VALUES
 ('수영', 'H-11'),
 ('피아노 연주', 'H-12');
 
--- MEM-HOBBY
+-- 회원정보 
+INSERT INTO mem_inform
+(NAME,AGE,PHONE,UNIQUE_ID,GENDER_ID,ADDRESS_ID)
+VALUES
+('홍길동',25,'010-1234-5678','M-01','G-03','A-01'),
+('김영희',30,'010-9876-5432','M-02','G-07','A-02'),
+('이철수',28,'010-5555-7777','M-03','G-03','A-03'),
+('박지원',23,'010-2222-3333','M-04','G-07','A-04'),
+('최성민',26,'010-4444-6666','M-05','G-03','A-05'),
+('김철호',32,'010-7777-8888','M-06','G-03','A-06'),
+('이지은',29,'010-9999-1111','M-07','G-07','A-07'),
+('정민준',27,'010-2222-3333','M-08','G-03','A-08');
+
+-- 회원 로그인
+INSERT INTO login
+(EMAIL,UNIQUE_ID,LOGIN_ID)
+VALUES
+('honggildong@gmail.com','M-01','L-01'),
+('kimyounghee@gmail.com','M-02','L-02'),
+('leechulsu@gmail.com','M-03','L-03'),
+('parkjiwon@gmail.com','M-04','L-04'),
+('choiseongmin@gmail.com','M-05','L-05'),
+('kimcheolho@gmail.com','M-06','L-06'),
+('leejiun@gmail.com','M-07','L-07'),
+('jeongminjun@gmail.com','M-08' ,'L-08');
+
+-- 회원 취미
 INSERT INTO mem_hobby
 (UNIQUE_ID,HOBBY_TYPE_ID)
 VALUES
@@ -85,6 +85,3 @@ VALUES
 ('M-07', 'H-08'),
 ('M-08', 'H-02'),
 ('M-08', 'H-04');
-
-
-
