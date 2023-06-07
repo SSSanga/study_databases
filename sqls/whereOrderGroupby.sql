cb31ebe220a97d098f6fea9ada1f0bf0eb03e89e
SSSanga <ksa901125@gmail.com>
2023-06-07 17:40:07 +0900

    employee number 표시

--- sqls/whereOrderGroupby.sql
+++ sqls/whereOrderGroupby.sql
@@ -1,6 +1,6 @@
 -- 조건: table Orders에서 EmployeeID가 7,9 직원의 각각 주문 갯수
 -- EmployeeID 7: 14개, EmployeeID 9:6개
-SELECT COUNT(*)
+SELECT COUNT(*), EmployeeID
 FROM Orders 
 WHERE EmployeeID IN (7, 9)
 GROUP BY EmployeeID;
