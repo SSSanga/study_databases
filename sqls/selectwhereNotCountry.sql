9360bb3a026eb5983d56b5ee2baa1f6f57f19aca
SSSanga <ksa901125@gmail.com>
2023-06-07 15:35:54 +0900

    UK가 아니라 USA임

--- sqls/selectwhereNotCountry.sql
+++ sqls/selectwhereNotCountry.sql
@@ -1,6 +1,10 @@
 SELECT * FROM Customers
+WHERE Country NOT IN ('Germany') AND Country NOT IN ('USA');
+-- RESULT Number of Records: 67
+-- UK가 아니라 USA 였음. 
+
 -- WHERE Country <> 'Germany' AND Country <> 'UK';
 -- -- Number of Records: 73
-SELECT * FROM Customers
-WHERE Country NOT IN ('Germany') AND Country NOT IN ('UK');
--- Number of Records: 73
+-- SELECT * FROM Customers
+-- WHERE Country NOT IN ('Germany') AND Country NOT IN ('UK');
+-- -- Number of Records: 73
