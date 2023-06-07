677e7702329c31737c1f6485d9accb0b362de892
SSSanga <ksa901125@gmail.com>
2023-06-07 15:53:50 +0900

    and가 많을때 ()로 묶음.

--- sqls/selectwhereRangeAndEquals.sql
+++ sqls/selectwhereRangeAndEquals.sql
@@ -1,5 +1,5 @@
 SELECT * FROM Customers
-WHERE CustomerID BETWEEN 50 AND 89  AND City IN ('London');
+WHERE CustomerID BETWEEN (50 AND 89)  AND City IN ('London');
 -- CustomerID가 50에서 89이고 City가 London인 고객
 -- Result 
 -- CustomerID: 53/ CustomerName: North/South /ContactName: Simon Crowther
