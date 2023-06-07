a4597a454bd2ed92af402ec95cddd620d203f136
SSSanga <ksa901125@gmail.com>
2023-06-07 15:30:41 +0900

    NOT IN으로 변경 IN 쓸때는 ()안에 넣어야함.

--- sqls/selectwhereNotCountry.sql
+++ sqls/selectwhereNotCountry.sql
@@ -1,3 +1,6 @@
 SELECT * FROM Customers
-WHERE Country <> 'Germany' AND Country <> 'UK';
+-- WHERE Country <> 'Germany' AND Country <> 'UK';
+-- -- Number of Records: 73
+SELECT * FROM Customers
+WHERE Country NOT IN ('Germany') AND Country NOT IN ('UK');
 -- Number of Records: 73
