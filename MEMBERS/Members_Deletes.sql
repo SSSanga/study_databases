a35aaee7d61e147210f022fc5d4dbe64b2f0c59a
SSSanga <ksa901125@gmail.com>
2023-06-07 12:33:01 +0900

    주석 수정

--- MEMBERS/Members_Deletes.sql
+++ MEMBERS/Members_Deletes.sql
@@ -1,5 +1,5 @@
 -- MEMBERS의 DB의 TABLE들에 대한 DATA를 다 삭제 하도록 하는 DML 작성
-
+-- members table의 column data만 사라지는것. 
 DELETE FROM membewrs_login
 WHERE MEMBERS_ID LIKE '%';
 
@@ -24,7 +24,7 @@ WHERE GENDER_CODE_ID;
 DELETE FROM members
 WHERE MEMBERS_ID;
 
-
+-- 이 아래 부분들은 table 자체들이 삭제되는것?
 -- DELETE FROM members_hobby;
 
 -- DELETE FROM hobby_code;
