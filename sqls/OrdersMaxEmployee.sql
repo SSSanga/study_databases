fa1af8ab30def1bdf875d02bc964130b04104313
SSSanga <ksa901125@gmail.com>
2023-06-08 16:51:23 +0900

    COUNT  MAX로 활성화시킴.

--- sqls/OrdersMaxEmployee.sql
+++ sqls/OrdersMaxEmployee.sql
@@ -11,19 +11,23 @@
 -- 그 첫번째 JOIN되는 가상의 TABLE을 t_TTORD
 -- 그 t_TTORD와 Employees TABLE을 JOIN해서 QUANTITY가 많은 직원 도출??
 
-SELECT MAX(t_TTORD.Quantity) AS MAX_QUANTITY,t_EMP.LastName,t_EMP.FirstName
-FROM 
-(SELECT *
-FROM OrderDetails AS t_DETAIL
-	INNER JOIN Orders AS t_ORDER
-    ON t_DETAIL.OrderID=t_ORDER.OrderID)AS t_TTORD 
-    	INNER JOIN Employees AS t_EMP
-		ON t_EMP.EmployeeID=t_TTORD.EmployeeID
+-- SELECT MAX(t_TTORD.Quantity) AS MAX_QUANTITY,t_EMP.LastName,t_EMP.FirstName
+-- FROM 
+-- (SELECT *
+-- FROM OrderDetails AS t_DETAIL
+-- 	INNER JOIN Orders AS t_ORDER
+--     ON t_DETAIL.OrderID=t_ORDER.OrderID)AS t_TTORD 
+--     	INNER JOIN Employees AS t_EMP
+-- 		ON t_EMP.EmployeeID=t_TTORD.EmployeeID
     
 -- RESULT 
 -- Number of Records: 1
 -- MAX_QUANTITY	LastName	FirstName
 -- 120	Fuller	Andrew
+
+-- 여기서 ordercount의 max를 쓰면 되는건가????
+-- 이 SELECT구문을 INNER로 만들고 MAX를 찾으면 되나봄. 
+
 -- Orders 테이블엔 OrderID, CustomerID, EmployeeID등 있음
 -- Orders테이블의 EmployeeID, , Employees테이블의 EmployeeID
 -- 1.여기서 주가 되는 table을 employees 테이블로 하자. 
@@ -39,15 +43,14 @@ FROM OrderDetails AS t_DETAIL
 -- 	ON t_EMP.EmployeeID = t_ORD.EmployeeID
 -- GROUP BY t_EMP.EmployeeID
 
--- 여기서 ordercount의 max를 쓰면 되는건가????
--- 이 SELECT구문을 INNER로 만들고 MAX를 찾으면 되나봄. 
--- SELECT MAX(t_COUNT.TOTAL_ORD) AS MAX_TOTAL_ORD,t_COUNT.LastName,t_COUNT.FirstName  
--- FROM
--- (SELECT COUNT(t_ORD.OrderID) AS TOTAL_ORD,t_EMP.LastName, t_EMP.FirstName 
--- FROM Employees AS t_EMP
---  	INNER JOIN Orders AS t_ORD
--- 	ON t_EMP.EmployeeID = t_ORD.EmployeeID
--- GROUP BY t_EMP.EmployeeID) AS t_COUNT;
+
+SELECT MAX(t_COUNT.TOTAL_ORD) AS MAX_TOTAL_ORD,t_COUNT.LastName,t_COUNT.FirstName  
+FROM
+(SELECT COUNT(t_ORD.OrderID) AS TOTAL_ORD,t_EMP.LastName, t_EMP.FirstName 
+FROM Employees AS t_EMP
+ 	INNER JOIN Orders AS t_ORD
+	ON t_EMP.EmployeeID = t_ORD.EmployeeID
+GROUP BY t_EMP.EmployeeID) AS t_COUNT;
 
 -- RESULT
 -- Number of Records: 1
